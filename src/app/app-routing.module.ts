e9ac79f07019896c44ffa2ad5e9b826ca5e1addd
Luiz Fernando Salles <luiz.salles@castinformatica.onmicrosoft.com>
2019-06-16 11:02:42 -0300

    Criação menu lateral com suas respectivas rotas de navegabilidade.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   { path: '', redirectTo: 'receita', pathMatch: 'full' },
   {
     path: 'receita',
@@ -15,15 +20,20 @@ const routes: Routes = [
         loadChildren: './receita/receita-detalhes/receita-detalhe.module#ReceitaDetalhePageModule'
       }
     ]
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
   }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
 
